0740c6989bb17c300460f5dd2bdaca44372487c4
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-10-26 17:39:04 -0500

    Help 2

--- src/LotteryOdds.java
+++ src/LotteryOdds.java
@@ -1,8 +1,8 @@
 
 
-public class LotteryOdds {
+public class LotteryOdds{ 
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		
 		int r;
 		int w = 10; // 69
@@ -10,20 +10,14 @@ public class LotteryOdds {
 		int pb = 26;
 	    r = factorial(w) / (factorial(s) * (factorial(w-s)));  // Superduper magic equation!!
 	    r = r * pb;
-	    
 	    System.out.println("The odds of winning the PowerBall are 1:" + r);
 	    System.out.println("The PowerBall website says that it's 1:292201338");
-	    
-    }
-
-	private static Integer factorial(int n) {
+		}	
+       	private static Integer factorial(int n){
 	    int i = 1;
 	    int f = 1;
-	 
-	    for (int c = 1; c <= n; c++) {
-	      f = f * i++;
+	    for (int c = 1; c <= n; c++){
+	    f = f * i++;
 	    }
-
-		return f;
-	}
-}
+	    return f;
+		}}
