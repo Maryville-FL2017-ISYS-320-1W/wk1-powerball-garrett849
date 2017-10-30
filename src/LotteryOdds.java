ec68031ae0eafd08f8f7da11292edf9d6782d5d0
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-10-29 21:17:54 -0500

    BigInteger

--- src/LotteryOdds.java
+++ src/LotteryOdds.java
@@ -4,13 +4,13 @@ public class LotteryOdds {
 
 	public static void main(String[] args) {
 
-		BigInteger r;
-		BigInteger w = new BigInteger("69"); // 69
-		BigInteger s = new BigInteger("5"); // 5
-		BigIntger pb = new BigInteger("26");
-	    r = factorial(w) divide (factorial(s) mulitipy (factorial(w-s)));  // Superduper magic equation!!
-	    r = r * pb;
-	  	System.out.println("The odds of winning the PowerBall are 1:" + r);
+		BigInteger answer;
+		BigInteger poolofwhiteballs = new BigInteger("69"); // 69
+		BigInteger pickedwhiteballs = new BigInteger("5"); // 5
+		BigInteger powerballs = new BigInteger("26");
+	    BigInteger answer = factorial(poolofwhiteballs.divide(factorial(pickedwhiteballs.mulitipy(factorial(poolofwhiteballs.subtract(pickedwhiteballs)));  // Superduper magic equation!!
+	    BigInteger answer = answer.mulitipy(powerballs);
+	  	System.out.println("The odds of winning the PowerBall are 1:" + answer)
 	    System.out.println("The PowerBall website says that it's 1:292201338");    
     }
 	private static Integer factorial(int n)  {
