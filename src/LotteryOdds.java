2f0c14a7cc4855ac97308796a6425cdd9a60cdb8
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-11-04 21:32:59 -0500

    powerball help

--- src/LotteryOdds.java
+++ src/LotteryOdds.java
@@ -5,18 +5,18 @@ public class LotteryOdds {
 	public static void main(String[] args) {
 
 		BigInteger r;
-		BigInteger w = new BigInteger("69"); // 69
-		BigInteger s = new BigInteger("5"); // 5
-		BigInteger pb = new BigInteger("26");
-	    r = factorial(w).divide (factorial(s).multiply (factorial(w.subtract(s))));  // Superduper magic equation!!
-	    r = r * pb;
+		int w = 69; // 69
+		int s = 5 ; // 5
+		int pb = 26;
+	    r = factorial(w).divide (factorial(s).multiply (factorial(w-s)));  // Superduper magic equation!!
+	    r = r.multiply(pb);
 	    
 	    System.out.println("The odds of winning the PowerBall are 1:" + r);
 	    System.out.println("The PowerBall website says that it's 1:292201338");
 	    
     }
 
-	private static Integer factorial(BigInteger w) {
+	private static BigInteger factorial(int w) {
 	    int i = 1;
 	    int f = 1;
 	 
